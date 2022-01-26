31229047ff805648d2f96af47d3637f742b0c801
JuanPalta <juanpalta24@hotmail.com>
2022-01-25 23:19:40 -0500

    feat: add icon,background, load file button and openFileDialog Functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,18 @@ namespace introduction_cs_municipalities
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loadDataButton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfileDialog = new OpenFileDialog();
+            if (openfileDialog.ShowDialog() == true) { 
+                 File.ReadLines(openfileDialog.FileName);
+
+        }
+        openfileDialog.Title = "Select file to load data";
+        openfileDialog.Filter = "Files csv(*.csv)|*.csv";
+        openfileDialog.InitialDirectory = @"introduction-cs-municipalities/data";
         }
     }
 }
